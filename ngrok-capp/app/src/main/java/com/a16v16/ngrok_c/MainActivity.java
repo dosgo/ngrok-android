216788fde06c824203407dd5da69d2425c56e47a
dosgo <dosgo@qq.com>
2016-05-12 17:12:53 +0800

    .

--- ngrok-capp/app/src/main/java/com/a16v16/ngrok_c/MainActivity.java
+++ ngrok-capp/app/src/main/java/com/a16v16/ngrok_c/MainActivity.java
@@ -1,17 +1,17 @@
 package com.a16v16.ngrok_c;
 
-import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import android.os.Handler;
+import java.lang.Runnable;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -20,31 +20,104 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class MainActivity extends AppCompatActivity {
-
+    String exec_path="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView tv=(TextView) findViewById(R.id.tv);
 
-        tv.setText( Build.CPU_ABI+":"+ MainActivity.this.getFilesDir());
+        tv.setText("未启动");
         try{
-           String exec_path=MainActivity.this.getFilesDir()+"/ngrokc";
-
-            Log.i("jni","0exec_path:"+exec_path);
+            exec_path=MainActivity.this.getFilesDir()+"/ngrokc";
             if(!fileIsExists(exec_path)) {
                 copyBigDataToSD(Build.CPU_ABI + "/ngrok-c", exec_path);
             }
             File exe_file = new File(exec_path);
             exe_file.setExecutable(true, true);
-            execCmd(exec_path+" -SER[Shost:tunnel.qydev.com,Sport:4443] -AddTun[Type:http,Lhost:192.168.2.116,Lport:80,Sdname:anddosgo]");
             //execCmd(exec_path);
         }catch (Exception  e){
 
             Log.i("jni","Exception："+e.getMessage());
         }
+        Button start=(Button) findViewById(R.id.start);
+        start.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                final EditText cmd=(EditText) findViewById(R.id.cmd);
+
+                    //execute the task
+                    Thread t1 = new Thread(new Runnable(){
+                        public void run(){
+                            try {
+                                execCmd(exec_path + " " + cmd.getText());
+                            } catch (Exception  e){
+                                e.printStackTrace();
+                                Log.i("jni","Exception："+e.getMessage());
+                            }
+                        }});
+                    t1.start();
+
+                    TextView tv=(TextView) findViewById(R.id.tv);;
+                    tv.setText("已启动");
+
+            }
+        });
+
+        Button reboot=(Button) findViewById(R.id.reboot);
+        start.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+
+
+
+
+                    Thread t = new Thread(new Runnable(){
+                        public void run(){
+                            try {
+                                DatagramSocket ds = new DatagramSocket();
+                                byte[] buf = "{\"cmd\":\"exit\"}".getBytes();
+                                DatagramPacket dp = new DatagramPacket(buf, buf.length, InetAddress.getByName("127.0.0.1"), 1885);//10000为定义的端口
+                                ds.send(dp);
+                                ds.close();
+                            } catch (Exception  e){
+                                    e.printStackTrace();
+                                    Log.i("jni","Exception："+e.getMessage());
+                                }
+                        }
+                    });
+                    t.start();
+
+                    new Handler().postDelayed(new Runnable(){
+                        public void run() {
+                            final EditText cmd=(EditText) findViewById(R.id.cmd);
+                            TextView tv=(TextView) findViewById(R.id.tv);;
+
+                                //execute the task
+                                Thread t1 = new Thread(new Runnable(){
+                                    public void run(){
+                                        try {
+                                        execCmd(exec_path + " " + cmd.getText());
+                                        } catch (Exception  e){
+                                            e.printStackTrace();
+                                            Log.i("jni","Exception："+e.getMessage());
+                                        }
+                                    }});
+                                t1.start();
+                                tv.setText("已启动");
+
+                        }
+                    }, 1000);
+
+
+            }
+        });
+
+
 
     }
     public boolean fileIsExists(String path){
@@ -71,18 +144,37 @@ public class MainActivity extends AppCompatActivity {
         String line = null;
         Log.i("jni","start1 ngrok-c");
         while (null != (line = br.readLine())) {
-            Log.i("jni","line:"+line);
             Log.e("########", line);
+            Message message=new Message();
+            Bundle bundle=new Bundle();
+            bundle.putString("line", line);
+            message.setData(bundle);//bundle传值，耗时，效率低
+            message.what=1;//标志是哪个线程传数据
+            myHandler.sendMessage(message);//发送message信息
         }
+        /*
         try {
-            process.waitFor();
+          //  process.waitFor();
         } catch (InterruptedException e) {
 
             e.printStackTrace();
-        }
-        Log.i("jni","end ngrok-c");
-
+        }*/
     }
+
+
+    Handler myHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 1:
+                    Bundle b = msg.getData();
+                    String line = b.getString("line");
+                    TextView out=(TextView) findViewById(R.id.out);;
+                    out.setText(out.getText()+line+"\r\n");
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+    };
 
     private void copyBigDataToSD(String src,String strOutFileName) throws IOException
     {
